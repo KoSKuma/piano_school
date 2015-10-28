ccae5349af61e9556947de7eb054fa10a5c60b84
fiizxwinchester <fiizx.winchester@gmail.com>
2015-10-28 15:12:37 +0700

    try responsive table

--- resources/views/teacherschedule/index.blade.php
+++ resources/views/teacherschedule/index.blade.php
@@ -2,7 +2,7 @@
 
 
 @section('htmlheader_title')
-
+Teacher Schedule
 @endsection
 
 
@@ -51,56 +51,61 @@
 		<div class="row">
 			<div class="col-sm-12" style="height:10px"></div>
 		</div>
-		<table class="table table-hover table-bordered" id="tabel-teacher-schedule">
-			<thead>
-				<tr>
-					<th bgcolor="#736F6E"><font color="white">Days/Time</font></th>
-					@foreach($time as $time_array)
-						<th  bgcolor="#736F6E">
-							<font color="white">{{$time_array}}</font>
-						</th>
-					@endforeach
-				</tr>
-			</thead>
-			<tbody>
-					@foreach($dateArray as  $key=>$day)
-						<tr>
-							<td bgcolor="#A0A0A0">	
-									<a href="{{url('schedule/create')}}/?teacher={{$teacher_id}}&day={{$key}}" class="link-color" ><b>{{$day}}</b></a>							      			
-							</td>
-						
-							@foreach($time as $time_key)
-							<?php 
-									$student_name ='';
-									$schedule_id = '';
-							?>		
-									
-
-							<?php	 if(isset($schedule_of_teacher[$day][$time_key]))
-									{	
-										$student_name = $schedule_of_teacher[$day][$time_key];  
-										$schedule_id = $schedules_id[$day][$time_key];
-
-										?>
-
-
-										<td onclick="document.location.href='{{url('schedule/'.$schedule_id.'/edit')}}/?teacher={{$teacher_id}}&day={{$key}}&time={{$time_key}}' " bgcolor="#C0D0FF" align="center"> {{$student_name}} </td>
-									
+		
+		<div class="table-responsive">
+			<table class="table table-hover table-bordered " id="tabel-teacher-schedule">
+				<thead>
+					<tr>
+						<th bgcolor="#736F6E"><font color="white">Days/Time</font></th>
+						@foreach($time as $time_array)
+							<th  bgcolor="#736F6E">
+								<font color="#ffffff">{{$time_array}}</font>
+							</th>
+						@endforeach
+					</tr>
+				</thead>
+				<tbody>
+						@foreach($dateArray as  $key=>$day)
+							<tr>
+								<td bgcolor="#A0A0A0">	
+										<a href="{{url('schedule/create')}}/?teacher={{$teacher_id}}&day={{$key}}" class="link-color" ><b>{{$day}}</b></a>							      			
+								</td>
+							
+								@foreach($time as $time_key)
+								<?php 
+										$student_name ='';
+										$schedule_id = '';
+								?>		
 										
-										
-							<?php   } 
-									else {
-									?>	<td bgcolor="#FFFFFF">{{$student_name}}</td>
-							<?php		}
 
-							?>
-								
-							@endforeach
-						</tr>
-					@endforeach
-					
-			</tbody>
-		</table>
+								<?php	 if(isset($schedule_of_teacher[$day][$time_key]))
+										{	
+											$student_name = $schedule_of_teacher[$day][$time_key];  
+											$schedule_id = $schedules_id[$day][$time_key];
+
+											?>
+
+
+											<td onclick="document.location.href='{{url('schedule/'.$schedule_id.'/edit')}}/?teacher={{$teacher_id}}&day={{$key}}&time={{$time_key}}' " bgcolor="#C0D0FF" align="center"> {{$student_name}} </td>
+										
+											
+											
+								<?php   } 
+										else {
+										?>	<td bgcolor="#FFFFFF">{{$student_name}}</td>
+								<?php		}
+
+								?>
+									
+								@endforeach
+							</tr>
+						@endforeach	
+				</tbody>
+			</table>
+		</div>
+		
+			
+		
 	</div>
 
 </div>
