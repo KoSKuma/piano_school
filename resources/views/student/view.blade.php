1fd56e98956b8a9af12c7ec59458a27db5a1c540
fiizxwinchester <fiizx.winchester@gmail.com>
2015-11-10 15:19:56 +0700

    Fix Back Button Teacher View Profile Mobile

--- resources/views/student/view.blade.php
+++ resources/views/student/view.blade.php
@@ -89,33 +89,7 @@ Add a new student
     </div>
 </div>
 
-<form action="" method="POST" id="confirm-delete"> 
-	<div class="modal fade " id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-					<h4 class="modal-title" id="myModalLabel">Delete Student</h4>
-				</div>
-				<div class="modal-body">
-					Are you sure you want to delete <span id="delete_message"></span>? 
-					<div class="modal-footer">
-						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-
-						<input type="hidden" name="_method" value="DELETE">
-						<input type="hidden" name="_token" value="{{ csrf_token() }}">
-						<button class="btn btn-danger" >
-							<span class="glyphicon glyphicon-remove-sign" aria-hidden="true"> </span> Delete
-						</button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-</form>
-	
 @endsection
-
 
 @section('script')
 @endsection
--- resources/views/teacher/view.blade.php
+++ resources/views/teacher/view.blade.php
@@ -17,14 +17,13 @@ Add a new student
             <form action="{{url('teacher/restore')}}" method="post">
                     {!! csrf_field() !!}
                     <!-- Single button -->
-                      <div class="col-sm-1 pull-left">
+                      <div class="col-xs-5 col-sm-1 pull-left ">
                         <a href= "{{url('teacher')}}" class="btn btn-flat btn-sm btn-default ">
                             <span class="glyphicon glyphicon-arrow-left"></span>
                              Back
                         </a>
                     </div>
-                    <div class="col-sm-11 text-right " >
-
+                    <div class="col-xs-7 col-sm-11 text-right">
                         <input type="hidden" name="_token" value="{{ csrf_token() }}">
                         <input type="hidden" name="id" id="delete_id" value="{{$teacher->id}}">
                     
@@ -101,72 +100,9 @@ Add a new student
 
     </div>
 </div>
-
   
-    <form action="" method="POST" id="confirm-delete"> 
-
-                <div class="modal fade " id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                                <h4 class="modal-title" id="myModalLabel">Delete Teacher</h4>
-                            </div>
-                            <div class="modal-body">
-                                Are you sure you want to delete <span id="delete_message"></span>? 
-                                <div class="modal-footer">
-                                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-
-                                    <input type="hidden" name="_method" value="DELETE">
-                                    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-                                    <button class="btn btn-danger" >
-                                        <span class="glyphicon glyphicon-remove-sign" aria-hidden="true"> </span> Delete
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </form>
-  
-
-                   
-    
-                         
-                        
-
-                        
-
-                
-  
+@endsection
 
 
-
-    @endsection
-
-
-    @section('script')
-    <!-- InputMask -->
-    <script src="{{url("plugins/input-mask/jquery.inputmask.js")}}" type="text/javascript"></script>
-    <script src="{{url("plugins/input-mask/jquery.inputmask.date.extensions.js")}}" type="text/javascript"></script>
-    <script src="{{url("plugins/input-mask/jquery.inputmask.extensions.js")}}" type="text/javascript"></script>
-    <script type="text/javascript">
-    $(function () {
-        $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
-        $("[data-mask]").inputmask();
-    });
-    <script type="text/javascript">
-
-$(document).ready(function(){
-    $('#myModal').on('shown.bs.modal',function(e){
-        $('#myInput').focus();
-        console.log(e);
-        delete_teacher_id = e.relatedTarget.attributes.teacher_id.value;
-        delete_teacher_name = e.relatedTarget.attributes.teacher_name.value;
-
-        $("#delete_message").html(delete_teacher_name);
-        $("#confirm-delete").attr("action", "{{url('teacher')}}"+"/"+delete_teacher_id);
-    });
-    </script>
-    @endsection
+@section('script')
+@endsection
